7b66ae3118257831745b229275684ae275b8e020
fivefootwan <wanvipa.wb@gmail.com>
2025-06-27 16:30:20 +0700

    done inserting qna

--- dopamine-menu/src/App.js
+++ dopamine-menu/src/App.js
@@ -11,48 +11,167 @@ function App() {
 
       <div className='Page-content'>
 
-        <div className='Quiz'>
+        <form className='Quiz'> {/*onSubmit={(e) => {e.preventDefault;}}*/}
           <p>Quiz</p>
 
           <div className='Question-card'>
-            <h2 className='Question'>This is a question:</h2>
+            <h2 className='Question'>Pick your setting</h2>
+              <div className='Options'>
+                <p className='Option'>
+                <input type="checkbox"/> 🛋️ Chill at home
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/> 🚶‍♀️ Go outside
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/> 🎧 Headphones on
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🕯️ Curl up in a cozy corner
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🗺️ Visit somewhere new (even nearby)
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🎨 Set up a creative zone
+                </p>
+
+                <p className='Custom-Option'>
+                ...Or choose your own
+                <input placeholder='e.g.dance'/>
+                </p>
+
+              </div>
+          </div>
+
+          <div className='Question-card'>
+            <h2 className='Question'>Choose your comfort move:</h2>
+              <div className='Options'>
+                <p className='Option'>
+                <input type="checkbox"/> 🍜 Eat
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>📱 Scroll
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🧸 Cuddle something
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🛏️ Make a nest of blankets
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🌀 Play with a fidget or stress toy
+                </p>
+
+                <p className='Option'>
+                <input type="checkbox"/>🌸 Smell something soothing (tea, candle)
+                </p>
+
+                <p className='Custom-Option'>
+                ...Or choose your own
+                <input placeholder='e.g.dance'/>
+                </p>
+              </div>
+          </div>
+
+          <div className='Question-card'>
+            <h2 className='Question'>Pick a mood booster</h2>
             <div className='Options'>
-              <button className='Option'>Option 1</button>
-              <button className='Option'>Option 2</button>
-              <button className='Option'>Option 3</button>
-              <button className='Option'>Option 4</button>
-              <button className='Option'>Option 5</button>
-              <button className='Option'>Option 6</button>
+              <p className='Option'>
+              <input type="checkbox"/>🎶 Music
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>✍️ Write
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>📞 Talk to someone
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>💃 Dance for a minute
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>🖌️ Doodle something
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>🌬️ Do 3 deep breaths
+              </p>
+
+              <p className='Custom-Option'>
+              ...Or choose your own
+              <input placeholder='e.g.dance'/>
+              </p>
             </div>
           </div>
 
           <div className='Question-card'>
-            <h2 className='Question'>This is a question:</h2>
+            <h2 className='Question'>Which feels most rewarding right now?</h2>
             <div className='Options'>
-              <button className='Option'>Option 1</button>
-              <button className='Option'>Option 2</button>
-              <button className='Option'>Option 3</button>
-              <button className='Option'>Option 4</button>
-              <button className='Option'>Option 5</button>
-              <button className='Option'>Option 6</button>
+              <p className='Option'>
+              <input type="checkbox"/>✅ Check something off
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>💡 Learn something
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>✨ Do nothing
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>🧹 Tidy a small area
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>🎁 Do a small kindness for someone
+              </p>
+
+              <p className='Option'>
+              <input type="checkbox"/>📓 Make a mini plan or list
+              </p>
+
+              <p className='Custom-Option'>
+              ...Or choose your own
+              <input placeholder='e.g.dance'/>
+              </p>
             </div>
           </div>
 
           <div className='Question-card'>
-            <h2 className='Question'>This is a question:</h2>
+            <h2 className='Question'>What’s your energy vibe?</h2>
             <div className='Options'>
-              <button className='Option'>Option 1</button>
-              <button className='Option'>Option 2</button>
-              <button className='Option'>Option 3</button>
-              <button className='Option'>Option 4</button>
-              <button className='Option'>Option 5</button>
-              <button className='Option'>Option 6</button>
+              <p className='Option'>
+              <input type="radio" name="energy"/>🔋 Full tank
+              </p>
+
+              <p className='Option'>
+              <input type="radio" name="energy"/>😐 Meh
+              </p>
+
+              <p className='Option'>
+              <input type="radio" name="energy"/>🪫 Running on fumes
+              </p>
+
             </div>
           </div>
 
+          <button type="submit" className='Submit-button'>Send your result</button>
 
-
-        </div>
+        </form>
 
         <div id='Menu-result'>
           <p>menu</p>
